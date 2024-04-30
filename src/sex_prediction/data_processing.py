53e597d9f355b7f7c7e6022cf47430274cb2964d
Yedidia AGNIMO <ayedidyas@gmail.com>
2024-04-30 23:36:01 +0200

    explorative data analysis

---
+++ src/sex_prediction/data_processing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def load_data(file_path):
+    # Load data from file_path (name frequency and transcriptions).
+
+    # Create total frequency column in name frequency data.
+
+    #
+    return pd.read_csv(file_path)
